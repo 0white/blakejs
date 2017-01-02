3695a2a741aad7bcc368baca2cc6a2dd5effe881
0white <thelastday@protonmail.com>
2017-01-02 19:48:57 +0300

    Typescript type definition added

---
+++ index.d.ts
@@ -0,0 +1,10 @@
+
+// Computes the BLAKE2B hash of a string or byte array, and returns a Uint8Array
+//
+// Returns a n-byte Uint8Array
+//
+// Parameters:
+// - input - the input bytes, as a string, Buffer or Uint8Array
+// - key - optional key Uint8Array, up to 64 bytes
+// - outlen - optional output length in bytes, default 64
+export function blake2b (input: string | Uint8Array, key?: Uint8Array, outlen?: number): Uint8Array;
